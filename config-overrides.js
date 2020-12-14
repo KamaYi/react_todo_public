e0a1cfd36e2502d8c0c45f6847d8213002be5651
limingyi <limingyi@58.com>
2020-12-14 17:08:23 +0800

    ant的样式自定义修改添加

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,23 @@
 /* config-overrides.js */
-const { override, fixBabelImports } = require("customize-cra");
+const { override, fixBabelImports, addLessLoader, addWebpackAlias, } = require("customize-cra");
+const path = require("path");
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
 module.exports = override(
     fixBabelImports("import", {
         libraryName: "antd",
         libraryDirectory: "es",
         style: "css"
+    }),
+    addLessLoader({
+        lessOptions: {
+            javascriptEnabled: true,
+            modifyVars: { '@primary-color': '#2abfe1cf' },
+        },
+    }),
+    // 配置路径别名
+    addWebpackAlias({
+        "@": resolve("src"),
     })
 );
