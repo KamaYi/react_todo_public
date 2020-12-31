66c98b694fe9174017aa939bbeff15e8be672fb0
limingyi <limingyi@58.com>
2020-12-31 18:08:54 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@ import { layoutRouteList } from './router/utils';
 import config from './config';
 import zhCN from "antd/es/locale/zh_CN";
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './styles/index.less';
 
 function App() {
   return (
+  <Provider store={store}>
     <ConfigProvider locale={zhCN}>
       <Suspense fallback={<Spin size="large" className="layout__loading" />}>
         <Router basename={config.BASENAME}>
@@ -26,6 +30,7 @@ function App() {
         </Router>
       </Suspense>
     </ConfigProvider>
+  </Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <ConfigProvider locale={zhCN}>
-    <App />
-  </ConfigProvider>,
+  <App />,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.register();
