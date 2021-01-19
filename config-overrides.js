d9a1d7ab595709a6bd67843d08ddf252b91a2b4e
limingyi <limingyi@58.com>
2021-01-19 18:40:39 +0800

    样式修改

--- config-overrides.js
+++ config-overrides.js
@@ -39,6 +39,23 @@ const devServerConfig = () => config => {
         }
     }
 }
+
+// 引入全局的less
+const lessGlobalConfiguration = () => config => {
+//修改、添加loader 配置 :
+        // 所有的loaders规则是在config.module.rules(数组)的第二项
+        // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
+        // 修改 less 配置 ，规则 loader 在第7项(具体可以打印配置)
+        const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
+        console.log(loaders)
+        loaders[7].use.push({
+            loader: 'style-resources-loader',
+            options: {
+                patterns: path.resolve(__dirname, 'src/assets/index.less')//全局引入公共的less 文件
+            }
+        })
+        return config
+}
 module.exports = {
     webpack: override(
         fixBabelImports("import", {
@@ -47,6 +64,7 @@ module.exports = {
             style: true //这里一定要写true，css和less都不行
         }),
         addLessLoader({
+            appendData: `@import "${ path.resolve('./src/assets/less/index.less') }";`, // 全局变量引入
             lessOptions: {
                 javascriptEnabled: true,
                 modifyVars: { '@primary-color': '#1DA57A' },
--- src/views/layout/Content/index.tsx
+++ src/views/layout/Content/index.tsx
@@ -4,21 +4,156 @@ import { Helmet } from 'react-helmet';
 import { Route, Switch } from 'react-router-dom';
 import { getPageTitle, routeList } from '@/router/utils';
 import { IRoute } from '@/router/config';
+import { Scrollbars } from 'react-custom-scrollbars';
 import './index.less'
 
 const LayoutContent = () => {
     const title = getPageTitle(routeList);
     console.log('routeList: ', routeList);
-    const { Content } = Layout;
     return (
-        <Content
-            className="layout-content"
-        >   <Helmet>
-                <title>{title}</title>
-                <meta name="description" content={title} />
-            </Helmet>
-            <div className="container">
-                <div className="content">
+        <div className="layout-content">
+            <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={200}>
+                <Helmet>
+                    <title>{title}</title>
+                    <meta name="description" content={title} />
+                </Helmet>
+                <div className="container">
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
+                    <span>asdasdasd</span>
                     asdasdasd
                     <Suspense fallback={<Spin className="layout__loading" />}>
                         <Switch>
@@ -28,8 +163,8 @@ const LayoutContent = () => {
                         </Switch>
                     </Suspense>
                 </div>
-            </div>
-        </Content>
+            </Scrollbars>
+        </div>
     );
 };
 export default LayoutContent;
