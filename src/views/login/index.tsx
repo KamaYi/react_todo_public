3ff49d89230a62bad0afec0e94c4ce888de66220
limingyi <limingyi@58.com>
2021-02-18 17:49:25 +0800

    文案修改

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { Input, Button, Form, Typography } from 'antd';
+import { Input, Button, Form, Typography, Row, Col } from 'antd';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setUserInfo, UserState } from '@/store/module/user';
@@ -54,42 +54,44 @@ function Login(props: LoginProps) {
   return (
     <div className="login-container">
       <div className="page-login">
-      <div className="top">
-        <Typography.Title className="header">
-          <Link to="/">
-            <span className="title">账号登录</span>
-          </Link>
-        </Typography.Title>
-      </div>
-      <Form
-        {...layout}
-        initialValues={{ remember: true }}
-        onFinish={onSubmit}
-        form={form}
-      >
-        <Form.Item
-          label="用户名："
-          name="account"
-          rules={[{ required: true, message: '请输入用户名!' }]}
+        <div className="top">
+          <Typography.Title className="header">
+            <Link to="/">
+              <span className="title">账号登录</span>
+            </Link>
+          </Typography.Title>
+        </div>
+        <Form
+          {...layout}
+          initialValues={{ remember: true }}
+          onFinish={onSubmit}
+          form={form}
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label="用户名："
+            name="account"
+            rules={[{ required: true, message: '请输入用户名!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="密码："
+            name="password"
+            rules={[{ required: true, message: '请输入密码!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item
-          label="密码："
-          name="password"
-          rules={[{ required: true, message: '请输入密码!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 3, span: 17 }}>
-          <Button block htmlType="submit" type="primary">
-            登录
+          <Form.Item wrapperCol={{ offset: 3, span: 17 }}>
+            <Button block htmlType="submit" type="primary">
+              登录
           </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          </Form.Item>
+        </Form>
+        <Row justify="center">
+          <Col span={14}>账号：随便填写-----------密码：111111</Col>
+        </Row>
+      </div>
     </div>
   );
 }
